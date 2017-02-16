a18f1ffb185f80ad262172b5f71da3b827e2f857
Konstantin S. <havendv@gmail.com>
2017-02-17 01:16:10 +1000

    Added base library without registration COM component

---
+++ DialogFormsSrc/DialogForms.h
@@ -0,0 +1,31 @@
+// DialogForms.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CDialogFormsApp:
+// See DialogForms.cpp for the implementation of this class
+//
+
+class CDialogFormsApp : public CWinApp
+{
+public:
+	CDialogFormsApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDialogFormsApp theApp;
---
+++ DialogFormsSrc/DialogFormsDlg.h
@@ -0,0 +1,40 @@
+// DialogFormsDlg.h : header file
+//
+
+#pragma once
+#include "afxwin.h"
+
+// CDialogFormsDlg dialog
+class CDialogFormsDlg : public CDialog
+{
+// Construction
+public:
+	CDialogFormsDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_DIALOGFORMS_DIALOG };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+	void OnMaskInputRejected(System::Object^, 
+		System::Windows::Forms::MaskInputRejectedEventArgs^);
+public:
+BEGIN_DELEGATE_MAP( CDialogFormsDlg )
+	EVENT_DELEGATE_ENTRY( OnMaskInputRejected, System::Object ^, 
+		System::Windows::Forms::MaskInputRejectedEventArgs^ )
+END_DELEGATE_MAP()
+
+// Implementation
+protected:
+	HICON m_hIcon;
+	//CWinFormsControl<System::Windows::Forms::MaskedTextBox> m_MaskedEdit;
+	CWinFormsControl<ImprovedControls::MyControl> m_MaskedEdit;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
